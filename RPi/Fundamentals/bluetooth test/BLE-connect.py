26e85dd45b93cb5be08bd2eae867a2e4155349d8
Chang Li <69972863+Lbaili@users.noreply.github.com>
2023-10-28 12:12:14 -0400

    Update BLE-connect.py

--- RPi/Fundamentals/bluetooth test/BLE-connect.py
+++ RPi/Fundamentals/bluetooth test/BLE-connect.py
@@ -1,37 +1,46 @@
-#!/usr/bin/env python
+from bluepy import btle
+import time
+import binascii
 
-from bluetooth.ble import GATTRequester
 
-address="CA:67:45:DC:EE:05"
-nrf=GATTRequester("CA:67:45:DC:EE:05", False)
-#nrf.write_by_handle(0x10, str(bytearray([14, 4, 56])))
-nrf.connect( wait=True, channel_type="public")
-print ("OK")
+# Address of BLE device to connect to.
+BLE_ADDRESS = "CA:67:45:DC:EE:05"
 
-# -*- mode: python; coding: utf-8 -*-
+BLE_SERVICE_UUID ="6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 
-#from __future__ import print_function
-#
-#import sys
-#from bluetooth.ble import GATTRequester
+BLE_CHARACTERISTIC_UUID= "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
-#
-#class JustConnect(object):
-#    def __init__(self, address):
-#        self.requester = GATTRequester(address, False)
-#        self.connect()
-#
-#    def connect(self):
-##        print("Connecting...", end=' ')
-##        sys.stdout.flush()
-#
-#        self.requester.connect(True)
-#        print("OK!")
-#
-#if __name__ == '__main__':
-#    if len(sys.argv) < 2:
-#        print("Usage: {} <addr>".format(sys.argv[0]))
-#        sys.exit(1)
+print("Connecting...")
+dev = btle.Peripheral(BLE_ADDRESS,"random")
 
-#    JustConnect(address)
-#    print("Done.")
+print("Services...")
+for svc in dev.services:
+    print(str(svc))
+
+class MyDelegate(btle.DefaultDelegate):
+    def __init__(self):
+        btle.DefaultDelegate.__init__(self)
+        # ... initialise here
+
+    def handleNotification(self, cHandle, data):
+    	data = bytearray(data)
+    	print('Developer: do what you want with the data.')
+    	print(data)
+
+
+
+
+
+dev.setDelegate( MyDelegate() )
+ 
+service_uuid = btle.UUID(BLE_SERVICE_UUID)
+ble_service = dev.getServiceByUUID(service_uuid)
+
+uuidConfig = btle.UUID(BLE_CHARACTERISTIC_UUID)
+#data_chrc = ble_service.getCharacteristics(uuidConfig)[1]
+
+while True:
+    if dev.waitForNotifications(1.0):
+        # handleNotification() was called
+        continue
+    print("Waiting...")
